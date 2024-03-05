65d70ebd05bbbe5e6baef2ca98fc3362193be85b
SNOW(Laptop) <rlatnsgh0708@postech.ac.kr(Laptop)>
2024-03-05 19:10:31 +0900

    completegit add .

--- alu/alu.v
+++ alu/alu.v
@@ -38,57 +38,72 @@ always @(*) begin
             C = A - B;
             
 			// 오버플로 처리 로직 추가
-			if(A[data_width-1] && B[data_width-1] && ~C[data_width-1])
-				OverflowFlag = 1; // Case1. A(양수) + B(양수) = C(음수)
-			else if(~A[data_width-1] && ~B[data_width-1] && C[data_width-1])
-				OverflowFlag = 1; // Case2. A(음수) + B(음수) = C(양수)
+			if(A[data_width-1] && ~B[data_width-1] && ~C[data_width-1])
+				OverflowFlag = 1; // Case1. A(양수) - B(음수) = C(음수)
+			else if(~A[data_width-1] && B[data_width-1] && C[data_width-1])
+				OverflowFlag = 1; // Case2. A(음수) - B(양수) = C(양수)
 			else
 				OverflowFlag = 0;
         end
         `FUNC_ID: begin
             C = A;
+            OverflowFlag = 0;
         end
         `FUNC_NOT: begin
             C = ~A;
+            OverflowFlag = 0;
         end
         `FUNC_AND: begin
             C = A & B;
+            OverflowFlag = 0;
         end
         `FUNC_OR: begin
             C = A | B;
+            OverflowFlag = 0;
         end
         `FUNC_NAND: begin
             C = ~(A & B);
+            OverflowFlag = 0;
         end
         `FUNC_NOR: begin
             C = ~(A | B);
+            OverflowFlag = 0;
         end
         `FUNC_XOR: begin
             C = A ^ B;
+            OverflowFlag = 0;
         end
         `FUNC_XNOR: begin
             C = ~(A ^ B);
+            OverflowFlag = 0;
         end
         `FUNC_LLS: begin
-            C = A << B;
+            C = A << 1;
+            OverflowFlag = 0;
         end
         `FUNC_LRS: begin
-            C = A >> B;
+            C = A >> 1;
+            OverflowFlag = 0;
         end
         `FUNC_ALS: begin
-            C = A <<< B;
+            C = A <<< 1;
+            OverflowFlag = 0;
         end
         `FUNC_ARS: begin
-            C = A >>> B;
+            C = $signed(A) >>> 1;
+            OverflowFlag = 0;
         end
         `FUNC_TCP: begin
             C = ~A + 1;
+            OverflowFlag = 0;
         end
         `FUNC_ZERO: begin
             C = 0;
+            OverflowFlag = 0;
         end
         default: begin
             C = 0;
+            OverflowFlag = 0;
         end
 	endcase
 end
