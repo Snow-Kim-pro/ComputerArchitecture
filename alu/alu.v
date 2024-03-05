7dff4231873a8fafe9cdc04e5cf38a4a299aee5d
SNOWKIM <rlatnsgh0708@postech.ac.kr>
2024-03-05 18:11:46 +0900

    Alu 완성 (but, 테스트벤치 안해봄)

--- alu/alu.v
+++ alu/alu.v
@@ -1,53 +1,16 @@
-module ALU #(parameter data_width = 16) (
+/* 2024.03.05 Snow version */
+`include "alu_func.v"
+
+module alu #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
 	input [3 : 0] FuncCode,
-    output reg [data_width - 1: 0] C,
-    output reg OverflowFlag);
-
+       	output reg [data_width - 1: 0] C,
+       	output reg OverflowFlag);
 // Do not use delay in your implementation.
+
 // You can declare any variables as needed.
-
 /* 	YOUR VARIABLE DECLARATION... */
-
-wire [data_width - 1: 0] add;
-wire [data_width - 1: 0] sub;
-wire [data_width - 1: 0] id;
-wire [data_width - 1: 0] not;
-wire [data_width - 1: 0] and;
-wire [data_width - 1: 0] or;
-wire [data_width - 1: 0] nand;
-wire [data_width - 1: 0] nor;
-wire [data_width - 1: 0] xor;
-wire [data_width - 1: 0] xnor;
-wire [data_width - 1: 0] lls;
-wire [data_width - 1: 0] lrs;
-wire [data_width - 1: 0] als;
-wire [data_width - 1: 0] ars;
-wire [data_width - 1: 0] tcp;
-wire [data_width - 1: 0] zero;
-wire overflowflag;
-
-assign add 	= A + B;
-assign sub 	= A - B;
-assign id  	= A;
-assign not 	= ~A;
-assign and 	= A & B;
-assign or 	= A | B;
-assign nand = ~(A & B);
-assign nor 	= ~(A | B);
-assign xor 	= A ^ B;
-assign xnor = A ~^ B;
-assign lls 	= A << 1;
-assign lrs 	= A >> 1;
-assign als 	= A <<< 1;
-assign ars 	= A >>> 1;
-assign tcp 	= ~A + 1;
-assign zero = 0;
-
-
-
-
 
 initial begin
 	C = 0;
@@ -56,30 +19,79 @@ end
 
 // TODO: You should implement the functionality of ALU!
 // (HINT: Use 'always @(...) begin ... end')
-/*
-	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
-*/
+/* 	YOUR ALU FUNCTIONALITY IMPLEMENTATION... */
 
 always @(*) begin
 	case(FuncCode)
-		FUNC_ADD : C = add;
-		FUNC_SUB : C = ;
-		FUNC_ID : 
-		FUNC_NOT :
-		FUNC_AND :
-		FUNC_OR :
-		FUNC_NAND :
-		FUNC_NOR :
-		FUNC_XOR :
-		FUNC_XNOR :
-		FUNC_LLS :
-		FUNC_LRS :
-		FUNC_ALS :
-		FUNC_ARS :
-		FUNC_TCP :
-		FUNC_ZERO :
-end
+        `FUNC_ADD: begin
+            C = A + B;
 
+            // 오버플로 처리 로직 추가
+			if(A[data_width-1] && B[data_width-1] && ~C[data_width-1])
+				OverflowFlag = 1; // Case1. A(양수) + B(양수) = C(음수)
+			else if(~A[data_width-1] && ~B[data_width-1] && C[data_width-1])
+				OverflowFlag = 1; // Case2. A(음수) + B(음수) = C(양수)
+			else
+				OverflowFlag = 0;
+        end
+        `FUNC_SUB: begin
+            C = A - B;
+            
+			// 오버플로 처리 로직 추가
+			if(A[data_width-1] && B[data_width-1] && ~C[data_width-1])
+				OverflowFlag = 1; // Case1. A(양수) + B(양수) = C(음수)
+			else if(~A[data_width-1] && ~B[data_width-1] && C[data_width-1])
+				OverflowFlag = 1; // Case2. A(음수) + B(음수) = C(양수)
+			else
+				OverflowFlag = 0;
+        end
+        `FUNC_ID: begin
+            C = A;
+        end
+        `FUNC_NOT: begin
+            C = ~A;
+        end
+        `FUNC_AND: begin
+            C = A & B;
+        end
+        `FUNC_OR: begin
+            C = A | B;
+        end
+        `FUNC_NAND: begin
+            C = ~(A & B);
+        end
+        `FUNC_NOR: begin
+            C = ~(A | B);
+        end
+        `FUNC_XOR: begin
+            C = A ^ B;
+        end
+        `FUNC_XNOR: begin
+            C = ~(A ^ B);
+        end
+        `FUNC_LLS: begin
+            C = A << B;
+        end
+        `FUNC_LRS: begin
+            C = A >> B;
+        end
+        `FUNC_ALS: begin
+            C = A <<< B;
+        end
+        `FUNC_ARS: begin
+            C = A >>> B;
+        end
+        `FUNC_TCP: begin
+            C = ~A + 1;
+        end
+        `FUNC_ZERO: begin
+            C = 0;
+        end
+        default: begin
+            C = 0;
+        end
+	endcase
+end
 
 endmodule
 
