46bbbd9bb3cd2c8e611eac384ff064e5319a0b2c
duhongk27 <duhongk27@postech.ac.kr>
2024-03-07 19:13:48 +0900

    duhong's alu

--- alu/alu.v
+++ alu/alu.v
@@ -1,58 +1,17 @@
-module ALU #(parameter data_width = 16) (
+`include "alu_func.v"
+
+module alu #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
 	input [3 : 0] FuncCode,
-    output reg [data_width - 1: 0] C,
-    output reg OverflowFlag);
-
+       	output reg [data_width - 1: 0] C,
+       	output reg OverflowFlag);
 // Do not use delay in your implementation.
+
 // You can declare any variables as needed.
-
-/* 	YOUR VARIABLE DECLARATION... */
-//숙숙숙
-wire [data_width - 1: 0] add;
-wire [data_width - 1: 0] sub;
-wire [data_width - 1: 0] id;
-wire [data_width - 1: 0] not;
-wire [data_width - 1: 0] and;
-wire [data_width - 1: 0] or;
-wire [data_width - 1: 0] nand;
-wire [data_width - 1: 0] nor; //여기있지롱
-wire [data_width - 1: 0] xor;
-wire [data_width - 1: 0] xnor;
-wire [data_width - 1: 0] lls;
-wire [data_width - 1: 0] lrs;
-wire [data_width - 1: 0] als;
-wire [data_width - 1: 0] ars;
-wire [data_width - 1: 0] tcp;
-wire [data_width - 1: 0] zero;
-wire overflowflag;
-wire please_delete_me_overflowff;
-
-
-// 안녕하세요. 이건 김순호가 수정한 코드입니다. 
-// 한글로안녕하세요
-
-assign add 	= A + B;
-assign sub 	= A - B;
-assign id  	= A;
-assign not 	= ~A;
-assign and 	= A & B;
-assign or 	= A | B;
-assign nand = ~(A & B);
-assign nor 	= ~(A | B);
-assign xor 	= A ^ B;
-assign xnor = A ~^ B;
-assign lls 	= A << 1;
-assign lrs 	= A >> 1;
-assign als 	= A <<< 1;
-assign ars 	= A >>> 1;
-assign tcp 	= ~A + 1;
-assign zero = 0;
-
-
-
-//텅빈거리홀로남아
+/*
+	YOUR VARIABLE DECLARATION...
+*/
 
 initial begin
 	C = 0;
@@ -64,27 +23,64 @@ end
 /*
 	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
 */
-//하이
+wire [data_width - 1 : 0] a0;
+wire [data_width - 1 : 0] a1;
+wire [data_width - 1 : 0] a2;
+wire [data_width - 1 : 0] a3;
+wire [data_width - 1 : 0] a4;
+wire [data_width - 1 : 0] a5;
+wire [data_width - 1 : 0] a6;
+wire [data_width - 1 : 0] a7;
+wire [data_width - 1 : 0] a8;
+wire [data_width - 1 : 0] a9;
+wire [data_width - 1 : 0] a10;
+wire [data_width - 1 : 0] a11;
+wire [data_width - 1 : 0] a12;
+wire [data_width - 1 : 0] a13;
+wire [data_width - 1 : 0] a14;
+wire [data_width - 1 : 0] a15;
+wire O0;
+wire O1;
+assign a0 = A + B;
+assign O0 = (A[data_width - 1] & B[data_width - 1] & ~a0[data_width - 1]) | (~A[data_width - 1] & ~B[data_width - 1] & a0[data_width - 1]) ;
+assign a1 = A - B;
+assign O1 = (A[data_width - 1] & ~B[data_width - 1] & ~a1[data_width - 1]) | (~A[data_width - 1] & B[data_width - 1] & a1[data_width - 1]) ;
+assign a2 = A;
+assign a3 = ~A;
+assign a4 = A & B;
+assign a5 = A | B;
+assign a6 = A ~& B;
+assign a7 = A ~| B;
+assign a8 = A ^ B;
+assign a9 = A ~^ B;
+assign a10 = A << 1;
+assign a11 = A >> 1;
+assign a12 = $signed(A) <<< 1;
+assign a13 = $signed(A) >>> 1;
+assign a14 = ~A + 1;
+assign a15 = 0;
+assign OverflowFlag = O0 | O1;
 always @(*) begin
-	case(FuncCode)
-		FUNC_ADD : C = add;
-		FUNC_SUB : C = ;
-		FUNC_ID : 
-		FUNC_NOT :
-		FUNC_AND :
-		FUNC_OR :
-		FUNC_NAND :
-		FUNC_NOR :
-		FUNC_XOR :
-		FUNC_XNOR :
-		FUNC_LLS :
-		FUNC_LRS :
-		FUNC_ALS :
-		FUNC_ARS :
-		FUNC_TCP :
-		FUNC_ZERO :
+	case (FuncCode)
+		0: C = a0;
+		1: C = a1;
+		2: C = a2;
+		3: C = a3;
+		4: C = a4;
+		5: C = a5;
+		6: C = a6;
+		7: C = a7;
+		8: C = a8;
+		9: C = a9;
+		10: C = a10;
+		11: C = a11;
+		12: C = a12;
+		13: C = a13;
+		14: C = a14;
+		15: C = a15;
+		default: C = 0;
+endcase
 end
 
-
 endmodule
-//흐흐 숨겨놨지롱.
+
