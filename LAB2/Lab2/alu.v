adde86b1b86d0b930c2692fb64dd06a59f20e1f5
duhongk27 <147086123+duhongk27@users.noreply.github.com>
2024-03-19 20:38:07 +0900

    duhong ver 1.1

--- LAB2/Lab2/alu.v
+++ LAB2/Lab2/alu.v
@@ -1,8 +1,66 @@
-module alu(alu_op, alu_in_1, alu_in_2, alu_result, alu_bcond);
-    input [6:0] alu_op;
-    input [31:0] alu_in_1;
-    input [31:0] alu_in_2;
-    output [31:0] alu_result;
-    output alu_bcond;
+module alu(alu_in_1, alu_in_2, alu_op, alu_result, alu_bcond);
+  input [31:0] alu_in_1;
+  input [31:0] alu_in_2;
+  input [3:0] alu_op;
+  output [31:0] alu_result;
+  output alu_bcond;
+
+  always (*) begin
+    case(opcode)
+      0:
+        alu_result = alu_in_1 + alu_in_2;
+      1:
+        alu_result = alu_in_1 - alu_in_2;
+      2:
+        alu_result = alu_in_1 & alu_in_2;
+      3:
+        alu_result = alu_in_1 | alu_in_2;
+      4:
+        alu_result = alu_in_1 ^ alu_in_2;
+      5:
+        alu_result = alu_in_1 << alu_in_2;
+      6:
+        alu_result = alu_in_1 >> alu_in_2;
+      default:
+        alu_result = 0;
+    endcase
+  end
+
+  always @(*) begin
+    case(opcode)
+      7: begin
+        if(alu_in_1 == alu_in_2) begin
+          alu_bcond = 1;
+        end else begin
+          alu_bcond = 0;
+        end
+      end
+      8: begin
+        if(alu_in_1 != alu_in_2) begin
+          alu_bcond = 1;
+        end else begin
+          alu_bcond = 0;
+        end
+      end
+      9: begin
+        if(alu_in_1 < alu_in_2) begin
+          alu_bcond = 1;
+        end else begin
+          alu_bcond = 0;
+        end
+      end
+      10: begin
+        if(alu_in_1 >= alu_in_2) begin
+          alu_bcond = 1;
+        end else begin
+          alu_bcond = 0;
+        end
+      end
+      default:
+        alu_bcond = 0;
+    endcase
+  end
+
 
 endmodule
+
---
+++ LAB2/Lab2/alu_control_unit.v
@@ -0,0 +1,57 @@
+module alu_control_unit(part_of_inst, alu_op)
+  input [31:0] part_of_inst;
+  output [3:0] alu_op;
+
+  wire sub_or_not = part_of_inst[30];
+  wire [2:0]funct3 = part_of_inst[14:12];
+  wire [6:0]opcode = part_of_inst[ 6: 0];
+
+  /*
+  add 0
+  sub 1
+  and 2
+  or 3
+  xor 4
+  sll 5
+  srl 6
+  beq 7
+  bne 8
+  blt 9
+  bge 10
+  */
+
+  always @(*) begin
+    if(opcode == 3'b1100011) begin
+      case(funct3)
+        3'b000:
+          alu_op = 7; //beq
+        3'b001:
+          alu_op = 8; //bne
+        3'b100:
+          alu_op = 9; //blt
+        default: //3'b101:
+          alu_op = 10; //bge
+      endcase
+    end else begin
+      case(funct3)
+        3'b000: begin
+          if(sub_or_not) begin
+            alu_op = 1;  //sub
+          end else begin
+            alu_op = 0; //add
+          end
+        end
+        3'b111:
+          alu_op = 2; //and
+        3'b110:
+          alu_op = 3; //or
+        3'b100:
+          alu_op = 4; //xor
+        3'b001:
+          alu_op = 5; //sll
+        default: //3'b101:
+          alu_op = 6;//srl
+      endcase
+    end   
+  end
+endmodule
