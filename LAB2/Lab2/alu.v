fbd2a893bf5c05eed99e42c0d398d87864a31187
SNOWKIM <rlatnsgh0708@postech.ac.kr>
2024-03-23 00:03:54 +0900

    상수 형식으로 변경 / sub(0), add(1)로 수정

--- LAB2/Lab2/alu.v
+++ LAB2/Lab2/alu.v
@@ -1,3 +1,4 @@
+
 module alu(alu_in_1, alu_in_2, alu_op, alu_result, alu_bcond);
   input [31:0] alu_in_1;
   input [31:0] alu_in_2;
@@ -8,9 +9,9 @@ module alu(alu_in_1, alu_in_2, alu_op, alu_result, alu_bcond);
   always @(*) begin
     case(alu_op)
       0:
+        alu_result = alu_in_1 - alu_in_2;    
+      1: 
         alu_result = alu_in_1 + alu_in_2;
-      1:
-        alu_result = alu_in_1 - alu_in_2;
       2:
         alu_result = alu_in_1 & alu_in_2;
       3:
@@ -29,38 +30,33 @@ module alu(alu_in_1, alu_in_2, alu_op, alu_result, alu_bcond);
   always @(*) begin
     case(alu_op)
       7: begin
-        if(alu_in_1 == alu_in_2) begin
+        if(alu_in_1 == alu_in_2) 
           alu_bcond = 1;
-        end else begin
+        else 
           alu_bcond = 0;
-        end
       end
       8: begin
-        if(alu_in_1 != alu_in_2) begin
+        if(alu_in_1 != alu_in_2)
           alu_bcond = 1;
-        end else begin
-          alu_bcond = 0;
-        end
+         else
+          alu_bcond = 0;        
       end
       9: begin
-        if(alu_in_1 < alu_in_2) begin
+        if(alu_in_1 < alu_in_2) 
           alu_bcond = 1;
-        end else begin
+        else 
           alu_bcond = 0;
-        end
       end
       10: begin
-        if(alu_in_1 >= alu_in_2) begin
+        if(alu_in_1 >= alu_in_2) 
           alu_bcond = 1;
-        end else begin
+        else 
           alu_bcond = 0;
-        end
       end
       default:
         alu_bcond = 0;
     endcase
   end
-
 
 endmodule
 
